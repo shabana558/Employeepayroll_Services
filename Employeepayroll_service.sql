f07fe3b01d21679db9763b1de020a990f6a9dcee
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-25 15:49:01 +0530

    [shabana] Duplicatevalue

--- Employeepayroll_service.sql
+++ Employeepayroll_service.sql
@@ -3,7 +3,7 @@
 
 EXEC sp_RENAME 'Employee_payrolls.Basic Pay' , 'BasicPay', 'COLUMN'
 Alter table Employee_payrolls
-add Deduction float,TaxablePay float, IncomeTax float,NetPay float;
+add Deduction float,TaxablePay float, IncomeTax float,NetPay float,BasicPay float;
 Update Employee_payrolls 
 set Deduction=1000
 where Gender='F';
@@ -14,4 +14,9 @@ update Employee_payrolls
 set NetPay=(BasicPay - Deduction)
 update Employee_payrolls
 set TaxablePay=0,IncomeTax=0
+select * from Employee_payrolls;
+
+------- UC 10: Adding another Value-------
+
+Insert into Employee_payrolls(name,BasicPay,StartDate,Address,EmployeePhoneNumber,EmployeeDepartment) values ('Terisa',250000,'2019-04-20','Chennai,TN','9600054540','Editing');
 select * from Employee_payrolls;
