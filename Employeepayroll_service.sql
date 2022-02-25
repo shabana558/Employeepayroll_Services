b4e2050b90f72fbcd980d42df3f9c506e86e24fc
shaik ameer basha <shaikshabana758@gmail.com>
2022-02-25 12:06:36 +0530

    [shabana]Table

--- Employeepayroll_service.sql
+++ Employeepayroll_service.sql
@@ -1,2 +1,17 @@
 --UC-1 creating database
 create database EmployeeServices;
+--UC-2 creating Table
+create Table Employee_payrolls
+(
+id int identity(1,1) primary key,
+name varchar(200) not null,
+salary float,
+startDate date
+)
+--UC-3 Insert values in Table
+Insert into Employee_payrolls(name,salary,startDate) values
+('shalini',20000,'2021-03-12'),
+('Magesh',25000,'2021-04-18'),
+('Gayathri',10000,'2020-05-13'),
+('Aruna',30000,'2020-08-19');
+select * from Employee_payrolls;
